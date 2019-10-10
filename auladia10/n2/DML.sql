3c22e6bd81325936072e1a9da7f4475bea0fe301
FIC <FIC@info.local>
2019-10-10 10:07:52 -0300

    n2

---
+++ auladia10/n2/DML.sql
@@ -0,0 +1,8 @@
+ INSERT INTO DIRETOR (Nome, RG)
+ VALUES ('Carlos' , 123344) , ('Lari' , 12345455) , ('Gusta' , 647899);
+
+
+ INSERT INTO DEPARTAMENTO (NomeDepartamento, Cargo , IdDiretor )
+ VALUES ('RH' , 'A' , 2), ('DEV' , 'B' , 2) , ('PRODUÇÃO' , 'C' , 1) ;
+
+ /* ADC DADOS NAS COLUNAS */
