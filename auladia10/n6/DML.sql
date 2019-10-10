152de2ce292d5f2e7196edb384606680e1b6db5b
FIC <FIC@info.local>
2019-10-10 11:31:52 -0300

    n6

---
+++ auladia10/n6/DML.sql
@@ -0,0 +1,3 @@
+INSERT INTO CURSO (Tipo) VALUES ('Code'), ('Espanhol') , ('dev');
+
+INSERT INTO CANDIDATO (Nome, RA, IdCurso) VALUES ('Isa' , '776588' , 1), ('Lari' , '222222' , 1), ('Nath' , '33333' , 3);
