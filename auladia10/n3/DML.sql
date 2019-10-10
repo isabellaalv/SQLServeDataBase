e519723bf08e7a22d975185569802faec778bf0e
FIC <FIC@info.local>
2019-10-10 10:38:35 -0300

    n3

---
+++ auladia10/n3/DML.sql
@@ -0,0 +1,5 @@
+INSERT INTO AUTOR (Nome) VALUES ('Carlos'), ('Machado'), ('Kiara');
+
+INSERT INTO LIVRO (DataPublicacao, Nome) VALUES ('19/10/10' , 'O amor Natural') , ('10/10/19' , 'Helena') , ('15/06/12', 'A Seleção');
+
+INSERT INTO PUPLICACAO (Idautor , IdLivro) VALUES (1,1), (2,2) , (3,3);
