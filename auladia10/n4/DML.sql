d41e0dc6c9a5a782c606a5acea44b3d31ca7a7b5
FIC <FIC@info.local>
2019-10-10 10:56:50 -0300

    n4

---
+++ auladia10/n4/DML.sql
@@ -0,0 +1,3 @@
+INSERT INTO EQUIPE (Tecnicos) VALUES ('Carille') , ('Jorge Jesus') , ('Fernando');
+
+INSERT INTO JOGADOR (IdEquipe , Nome) VALUES ( 1, 'Pedrinho') , ( 2, 'Gusta Gol') , (3 ,'Hernandes');
